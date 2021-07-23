ff2ffb12b776720e07b50a7bf3e583739a7917b4
James Gibson <james.gibson@bbc.co.uk>
2021-07-23 15:40:43 +0100

    Use state to determine if VM delete
    Depends-On: https://review.opendev.org/c/vexxhost/atmosphere/+/794569
    
    Change-Id: I9b85fdac225f1f198cba1574c027e94cc09e057c

--- atmosphere/models.py
+++ atmosphere/models.py
@@ -251,11 +251,15 @@ class Instance(Resource):
         """is_event_ignored"""
         vm_state_is_deleted = (event['traits']['state'] == 'deleted')
         no_deleted_at = ('deleted_at' not in event['traits'])
-
         if vm_state_is_deleted and no_deleted_at:
             return True
 
-        # Check if event is missing both created_at and launched_at traits
+        # Ignore compute.instance.updates events
+        if event['event_type'].startswith('compute.instance.update'):
+            return True
+
+        # Check if event is missing launched_at traits
+        # Means building state is skipped
         no_created_at = ('created_at' not in event['traits'])
         no_launched_at = ('launched_at' not in event['traits'])
         if no_created_at and no_launched_at:
@@ -266,7 +270,9 @@ class Instance(Resource):
     @classmethod
     def is_event_delete(cls, event):
         """is_event_delete"""
-        return 'deleted_at' in event['traits']
+
+        return ('deleted_at' in event['traits'] or
+                event['traits']['state'] == 'deleted')
 
 
 class Volume(Resource):
